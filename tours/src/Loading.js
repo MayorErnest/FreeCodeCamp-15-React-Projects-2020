045c17124945228fcceeb2bac682c021ab50e72d
Eagle <eobumma@gmail.com>
2020-12-29 05:48:07 +0100

    finish loading component

---
+++ tours/src/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoadingImg from "./Assets/Loading.gif";
+
+function Loading() {
+  return (
+    <>
+      <img src={LoadingImg} alt="loading Image" />
+    </>
+  );
+}
+
+export default Loading;
