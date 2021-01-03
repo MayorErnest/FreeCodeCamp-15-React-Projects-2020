84a2745a78a25b91911671e447990ca91f5a69b8
MayorErnest <eobumma@gmail.com>
2021-01-03 07:12:05 +0100

    add app logic to accordion app

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import data from "./data";
+import Question from "./Question"
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <main>
+      <section className="col-1">
+        <h1>Questions And Answers About Login</h1>
+      </section>
+      <section className="col-2">
+        {data.map((item)=> <Question  key={item.id} title={item.title} info={item.info}/>)}
+      </section>
+    </main>
   );
 }
 
