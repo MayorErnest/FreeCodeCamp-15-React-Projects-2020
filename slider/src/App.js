2322c7be71b6d91bada6744c4dac7c640cf9cc01
MayorErnest <eobumma@gmail.com>
2021-01-07 15:17:41 +0100

    initialize and setup slider app

---
+++ slider/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <main className="App">
+      <header>
+        <h1>Reviews</h1>
+      </header>
+    </main>
+  );
+}
+
+export default App;
