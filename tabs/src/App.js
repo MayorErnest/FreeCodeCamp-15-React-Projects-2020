4a56c9ef7a49887c2c1af8204b75252c037bd3df
MayorErnest <eobumma@gmail.com>
2021-01-07 03:43:39 +0100

    add app logic to tabs app

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -1,10 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
+const url = "https://course-api.com/react-tabs-project";
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [index, setIndex] = useState(0);
+
+  const fetchData = async () => {
+    let response = await fetch(url);
+    let responseJson = await response.json();
+    setData(responseJson);
+    setLoading(false);
+  };
+
+  useEffect(() => fetchData(), []);
+
+  const { title, dates, duties, company } = data[index];
   return (
-    <div className="App">
-    </div>
+    <main className="App">
+      <header>
+        <h1>Experience</h1>
+      </header>
+      <aside>
+        {data.map((item, index) => (
+          <button key={item.id} onClick={() => setIndex(index)}>
+            {item.company}
+          </button>
+        ))}
+      </aside>
+      <section>
+        <article>
+          <h2>{title}</h2>
+          <h4>{company}</h4>
+          <h5>{dates}</h5>
+          {duties.map((duty, index) => (
+            <p key={index}>{duty}</p>
+          ))}
+        </article>
+      </section>
+    </main>
   );
 }
 
