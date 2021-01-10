0bebdab1fcc8dbe6a46b164ff8156a64547f6b21
MayorErnest <eobumma@gmail.com>
2021-01-10 15:58:19 +0100

    add functionality and logic to lorem ipsum app

--- lorem-ipsum/src/App.js
+++ lorem-ipsum/src/App.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data";
 
+console.log(data.length);
 function App() {
-  return <div className="App"></div>;
+  const [numbers, setNumbers] = useState(0);
+  const [paragraphs, setParagraphs] = useState([]);
+
+  const handleNumber = (num) => (num <= 0 ? 1 : num);
+
+  const handleChange = (event) => {
+    setNumbers(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setParagraphs(data.slice(0, handleNumber(numbers)));
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="paragraph">Paragraph</label>
+        <input
+          type="number"
+          id="paragraph"
+          value={numbers}
+          onChange={handleChange}
+        />
+        <button type="submit">Generate</button>
+      </form>
+      <section>
+        {paragraphs.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </section>
+    </div>
+  );
 }
 
 export default App;
